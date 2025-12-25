1d636553816c0817ce1e00da5c3b2035eeb1342d
Lucas Nascimento <2006lucasns@gmail.com>
2025-12-25 17:35:31 -0300

    refactor: pagina de previsao responsividade e cor do modal

--- src/pages/Previsao.jsx
+++ src/pages/Previsao.jsx
@@ -4,10 +4,10 @@ import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/re
 const Previsao = () => {
     const [open, setOpen] = useState(false)
     return (
-        <div className="h-screen bg-[#e3e5f0] flex flex-col justify-center items-center">
+        <div className="min-h-screen bg-[#e3e5f0] flex flex-col justify-center items-center px-2 py-4">
             <div className="flex flex-col justify-center items-center md:flex-row">
                 <div className="bg-white w-sm rounded-2xl p-4 shadow-2xl">
-                    <p className="text-center text-3xl w-sm font-bold py-2">Entrada</p>
+                    <p className="text-center text-3xl font-bold py-2">Entrada</p>
                     <form
                         onSubmit={(e) => {
                             e.preventDefault()
@@ -60,25 +60,25 @@ const Previsao = () => {
                                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                                             <DialogPanel
                                                 transition
-                                                className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in  data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                                                className="relative transform overflow-hidden rounded-lg bg-blue-50 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in  data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                                             >
-                                                <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                                     <div className="sm:flex sm:items-start">
                                                         <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                            <DialogTitle as="h3" className="text-base font-semibold text-white">
+                                                            <DialogTitle as="h3" className="text-base font-semibold text-black">
                                                                 Resultado
                                                             </DialogTitle>
                                                             <div className="mt-2">
-                                                                <p className="text-sm text-gray-400">
+                                                                <p className="text-sm text-gray-900">
                                                                     Previsão:
                                                                 </p>
-                                                                <p className="text-sm text-gray-400">
+                                                                <p className="text-sm text-gray-900">
                                                                     Probabilidade:
                                                                 </p>
-                                                                <p className="text-sm text-gray-400">
+                                                                <p className="text-sm text-gray-900">
                                                                     Nivel de risco:
                                                                 </p>
-                                                                <p className="text-sm text-gray-400">
+                                                                <p className="text-sm text-gray-900">
                                                                     Recomendação:
                                                                 </p>
                                                             </div>
