f0eada5c05ef55f3c968029889ea59cf9ea19c49
Lucas Nascimento <2006lucasns@gmail.com>
2025-12-26 15:52:13 -0300

    refactor: formulario de previsao com dropdown menu

--- src/pages/Previsao.jsx
+++ src/pages/Previsao.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { ChevronDownIcon } from '@heroicons/react/16/solid'
 const Previsao = () => {
     const [open, setOpen] = useState(false)
+    const [pais, setPais] = useState('')
+    const [sexo, setSexo] = useState('')
     return (
-        <div className="min-h-screen bg-[#e3e5f0] flex flex-col justify-center items-center px-2 py-4">
+        <div className="min-h-screen bg-[#e3e5f0] flex flex-col justify-center items-center px-2 py-8 sm:py-4">
             <div className="flex flex-col justify-center items-center md:flex-row">
                 <div className="bg-white w-sm rounded-2xl p-4 shadow-2xl">
                     <p className="text-center text-3xl font-bold py-2">Entrada</p>
@@ -16,37 +19,79 @@ const Previsao = () => {
                     >
                         <div class="mb-6">
                             <label class="block mb-2.5 text-sm font-medium text-heading">Score do cartão de crédito</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                            <input type="number" placeholder='Score do cartão de crédito' class="rounded-md bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:outline-green-400 block w-full px-3 py-2.5 shadow-sm placeholder:text-body" required />
                         </div>
-                        <div class="mb-6">
-                            <label class="block mb-2.5 text-sm font-medium text-heading">País</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                        <div className="mb-6 sm:col-span-3">
+                            <label htmlFor="pais" className="block mb-2.5 text-sm font-medium text-heading text-black">
+                                País
+                            </label>
+                            <div className="mt-2 grid grid-cols-1">
+                                <select
+                                    id="pais"
+                                    name="pais"
+                                    value={pais}
+                                    onChange={(e) => setPais(e.target.value)}
+                                    autoComplete="pais-name"
+                                    required
+                                    className="border border-default-medium col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-black outline-1 -outline-offset-1 outline-white/10 *:bg-white focus:outline-2 focus:-outline-offset-2 focus:outline-green-400 shadow-sm sm:text-sm/6"
+                                >
+                                    <option value="" disabled hidden></option>   
+                                    <option>França</option>
+                                    <option>Alemanha</option>
+                                    <option>Espanha</option>
+                                </select>
+                                <ChevronDownIcon
+                                    aria-hidden="true"
+                                    className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-black sm:size-4"
+                                />
+                            </div>
                         </div>
-                        <div class="mb-6">
-                            <label class="block mb-2.5 text-sm font-medium text-heading">Gênero</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                        <div className="mb-6 sm:col-span-3">
+                            <label htmlFor="sexo" className="block mb-2.5 text-sm font-medium text-heading text-black">
+                                Sexo
+                            </label>
+                            <div className="mt-2 grid grid-cols-1">
+                                <select
+                                    id="sexo"
+                                    name="sexo"
+                                    value={sexo}
+                                    onChange={(e) => setSexo(e.target.value)}
+                                    autoComplete="sexo-name"
+                                    required
+                                    className="border border-default-medium col-start-1 row-start-1 w-full appearance-none rounded-md bg-white/5 py-1.5 pr-8 pl-3 text-base text-black outline-1 -outline-offset-1 outline-white/10 *:bg-white focus:outline-2 focus:-outline-offset-2 focus:outline-green-400 shadow-sm sm:text-sm/6"
+                                >
+                                    <option value="" disabled hidden></option> 
+                                    <option>Feminino</option>
+                                    <option>Masculino</option>
+                                </select>
+                                <ChevronDownIcon
+                                    aria-hidden="true"
+                                    className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-black sm:size-4"
+                                />
+                            </div>
                         </div>
+                        {/* ---- */}
                         <div class="mb-6">
                             <label class="block mb-2.5 text-sm font-medium text-heading">Idade</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                            <input type="number" placeholder='Idade' class="rounded-md bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:outline-green-400 block w-full px-3 py-2.5 shadow-sm placeholder:text-body" required />
                         </div>
                         <div class="mb-6">
                             <label class="block mb-2.5 text-sm font-medium text-heading">Tempo de trabalho (meses)</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                            <input type="number" placeholder='Tempo de trabalho (meses)' class="rounded-md bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:outline-green-400 block w-full px-3 py-2.5 shadow-sm placeholder:text-body" required />
                         </div>
                         <div class="mb-6">
                             <label class="block mb-2.5 text-sm font-medium text-heading">Balance</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                            <input type="number" placeholder='Balance' class="rounded-md bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:outline-green-400 block w-full px-3 py-2.5 shadow-sm placeholder:text-body" required />
                         </div>
                         <div class="mb-6">
                             <label class="block mb-2.5 text-sm font-medium text-heading">Salário estimado</label>
-                            <input type="text" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                            <input type="number" placeholder='Salário estimado' class="rounded-md bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:outline-green-400 block w-full px-3 py-2.5 shadow-sm placeholder:text-body" required />
                         </div>
                         <div className="flex justify-center">
                             <div>
                                 <button
                                     type="submit"
-                                    className="text-white hover:cursor-pointer hover:scale-105 bg-linear-to-r from-[#0077FF] to-[#39EA29] shadow-xs font-medium leading-5 rounded-sm text-xl px-4 py-2.5 focus:outline-none"
+                                    className="text-white hover:cursor-pointer hover:scale-105 bg-linear-to-r from-[#0077FF] to-[#39EA29] shadow-sm font-medium leading-5 rounded-sm text-xl px-4 py-2.5 focus:outline-none"
                                 >
                                     Prever
                                 </button>
