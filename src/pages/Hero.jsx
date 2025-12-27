f57860873869d6415ce0ad766a679e0dead1397f
Lucas Nascimento <2006lucasns@gmail.com>
2025-12-27 20:18:51 -0300

    refactor: design da hero section

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import ParticlesBackground from "../components/ParticlesBackground";
+import { ArrowTrendingUpIcon } from "@heroicons/react/16/solid";
 
 export default function Hero() {
     return (
@@ -10,13 +11,16 @@ export default function Hero() {
             <div className="mx-auto max-w-4xl px-6 py-16">
                 <div className="relative isolate overflow-hidden backdrop-blur-sm rounded-3xl px-8 py-14 flex flex-col text-center justify-center items-center ">
 
-                    <h1 className="w-fit font-bold text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl tracking-tight mb-6 bg-linear-to-r from-[#0077FF] to-[#39EA29] bg-clip-text text-transparent ">
+                    {/* <h1 className="w-fit font-bold text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xl tracking-tight mb-6 bg-linear-to-r from-[#0077FF] to-[#39EA29] bg-clip-text text-transparent ">
                         CHURN INSIGHT
+                    </h1> */}
+                    <div className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-blue-600/25  text-primary-foreground mb-6 border border-blue-600/30">
+                        <ArrowTrendingUpIcon className="w-4 h-4 text-white" />
+                        <span className="text-sm font-medium text-white">Previsão de Churn</span>
+                    </div>
+                    <h1 className="w-fit font-bold text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-7xlfont-semibold tracking-tight text-white">
+                        Preveja Cancelamentos e <span className="bg-linear-to-r from-[#0077FF] to-[#39EA29] bg-clip-text text-transparent">Retenha Mais Clientes</span>
                     </h1>
-
-                    <h2 className="text-xl sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl font-semibold tracking-tight text-white">
-                        Decida antes que o cliente decida sair.
-                    </h2>
 
                     <p className="mt-6 text-lg sm:text-lg md:text-lg lg:text-xl xl:text-xl text-gray-300">
                         Identifique clientes com alto risco de cancelamento usando dados de comportamento e histórico de uso, permitindo ações de retenção antes que seja tarde.
@@ -27,7 +31,7 @@ export default function Hero() {
                             to="/previsao"
                             className="rounded-md bg-linear-to-r from-[#0077FF] to-[#39EA29] px-4 py-2.5 text-xl font-semibold text-white hover:scale-110 transition-all focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                         >
-                            Prever
+                            Fazer previsão
                         </Link>
                     </div>
                 </div>
--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://churninsight-backend-h12-25b.onrender.com/',
+    // baseURL: 'https://churninsight-backend-h12-25b.onrender.com/',
+    baseURL: 'http://localhost:8080/',
 });
 
 export default api;
